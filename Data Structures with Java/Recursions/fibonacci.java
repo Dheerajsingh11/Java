b7bf9edfbc2165b1cfeeead25e55b6d333c7d3c3
Dheeraj Singh <dheerajsinghchauhan05@gmail.com>
2024-07-26 23:51:36 +0530

    Data Structure: Recursion Basic codes

---
+++ Data Structures with Java/Recursions/fibonacci.java
@@ -0,0 +1,16 @@
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fib(n));
+    }
+
+    static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
