48b67a1bf0ead6b5afbb16f16bc329e698e4129e
Dheerajsingh11 <49749368+Dheerajsingh11@users.noreply.github.com>
2024-08-08 00:23:22 +0530

    [Data Structures]: Searching - Adding basic searching algorithms

---
+++ Data Structures with Java/Searching/binarySearchIterative.java
@@ -0,0 +1,23 @@
+public class binarySearchIterative {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println(binarySearch(arr, arr.length, 8));
+    }
+
+    static int binarySearch(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == x) {
+                return mid + 1;
+            } else if (arr[mid] > x) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+// Time complexity: O(log(n))
+// Auxiliary Space: O(1)
---
+++ Data Structures with Java/Searching/binarySearchRecursive.java
@@ -0,0 +1,20 @@
+public class binarySearchRecursive {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println(binarySearch(arr, 0, arr.length, 8));
+    }
+
+    static int binarySearch(int[] arr, int low, int high, int x) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = (low + high) / 2;
+        if (arr[mid] == x) {
+            return mid;
+        } else if (arr[mid] > x) {
+            return binarySearch(arr, low, mid - 1, x);
+        } else {
+            return binarySearch(arr, mid + 1, high, x);
+        }
+    }
+}
